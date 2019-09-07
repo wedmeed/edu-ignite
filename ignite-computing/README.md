ef58099735cbd3623052289041d3b43ea27bbc43
asokolov <wedmeed@mail.ru>
2019-09-07 13:39:44 +0400

    refactor configs to work in cluster

--- ignite-computing/README.md
+++ ignite-computing/README.md
@@ -10,3 +10,14 @@ docker run --name bootignite -e "CONFIG_URI=file:///tmp/config.xml" -v "/Users/a
 
 gradlew dockerBuildImage
 docker run --name myignite -e "CONFIG_URI=file:///tmp/config.xml" -v "/Users/asokolov/Desktop/docker/ignite-storage":/tmp apacheignite:ignite
+
+
+
+
+there are some possible bugs with cassandra persistence storage:
+http://apache-ignite-users.70518.x6.nabble.com/Getting-NullPointerException-during-commit-into-cassandra-after-reconnecting-to-ignite-server-td22005.html
+
+also peer class loading works only for computing:
+https://www.gridgain.com/resources/blog/apacher-ignitetm-tip-peer-class-loading-deployment-magic
+
+
--- ignite-computing/src/main/java/edu/ignite/computing/config/CacheConfig.java
+++ ignite-computing/src/main/java/edu/ignite/computing/config/CacheConfig.java
@@ -1,68 +1,26 @@
 package edu.ignite.computing.config;
 
-import com.datastax.driver.core.policies.RoundRobinPolicy;
-import edu.ignite.computing.model.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
-import org.apache.ignite.cache.CacheAtomicityMode;
-import org.apache.ignite.cache.store.cassandra.CassandraCacheStoreFactory;
-import org.apache.ignite.cache.store.cassandra.datasource.DataSource;
-import org.apache.ignite.cache.store.cassandra.persistence.KeyValuePersistenceSettings;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.transactions.TransactionConcurrency;
 import org.apache.ignite.transactions.spring.SpringTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.cache.expiry.CreatedExpiryPolicy;
-import javax.cache.expiry.Duration;
-import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 
 @Configuration
 @Slf4j
 public class CacheConfig {
 
-    @Bean
-    public CacheConfiguration<String, Product> cacheConfiguration() {
-        CacheConfiguration<String, Product> ccfg = new CacheConfiguration<>("ProductCache");
-        ccfg.setIndexedTypes(String.class, Product.class);
-        DataSource dataSource = new DataSource();
-        dataSource.setContactPoints("127.0.0.1");
-        RoundRobinPolicy robinPolicy = new RoundRobinPolicy();
-        dataSource.setLoadBalancingPolicy(robinPolicy);
-        dataSource.setReadConsistency("ONE");
-        dataSource.setWriteConsistency("ONE");
-        dataSource.setJmxReporting(true);
-        KeyValuePersistenceSettings persistenceSettings = new KeyValuePersistenceSettings(new File(getClass().getResource("/persistence_settings.xml").getFile()));
-        CassandraCacheStoreFactory<String, Product> cacheStoreFactory = new CassandraCacheStoreFactory<>();
-        cacheStoreFactory.setDataSource(dataSource);
-        cacheStoreFactory.setPersistenceSettings(persistenceSettings);
-        ccfg.setCacheStoreFactory(cacheStoreFactory);
-
-        ccfg.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
-
-        ccfg.setWriteThrough(true);
-
-        ccfg.setReadThrough(true);
-        ccfg.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, 70)));
-
-        ccfg.setStatisticsEnabled(true);
-
-        return ccfg;
-    }
 
     /**
      * Creating Apache Ignite instance bean. A bean will be passed
      * to IgniteRepositoryFactoryBean to initialize all Ignite based Spring Data      * repositories and connect to a cluster.
      */
     @Bean
-    public Ignite igniteInstance(CacheConfiguration<String, Product> ccfg) {
-        Ignite ignite = Ignition.start("./config.xml");
-        ignite.getOrCreateCache(ccfg);
-
+    public Ignite igniteInstance() {
+        Ignite ignite = Ignition.start("file:///Users/asokolov/Desktop/edu/ignite/ignite-computing/src/main/resources/config.xml");
         return ignite;
     }
 
